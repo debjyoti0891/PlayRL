f3d2c3f316e9432a798b51d5896860f120da9001
debjyoti0891 <debjyoti08911@gmail.com>
2019-12-03 17:25:25 +0800

    NN based Q table attempt in progress

---
+++ taxiNNQTable.py
@@ -0,0 +1,183 @@
+import gym
+import numpy as np
+import random
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+# Implements Q learning 
+
+# Q-table is approximated using a NN
+# https://medium.com/emergent-future/simple-reinforcement-learning-with-tensorflow-part-0-q-learning-with-tables-and-neural-networks-d195264329d0
+
+env = gym.make("Taxi-v3").env
+
+env.render()
+
+print("Action Space {}".format(env.action_space))
+print("State Space {}".format(env.observation_space))
+
+state = env.encode(3, 1, 2, 0) # (taxi row, taxi column, passenger index, destination index)
+print("State:", state)
+
+env.s = state
+env.render()
+# encoding of reward table: {action: [(probability, nextstate, reward, done)]}
+print('reward values: {}'.format(env.P[328]))
+
+#Qtable : state,action combo 
+tf.reset_default_graph()
+
+#These lines establish the feed-forward part of the network used to choose actions
+inputs1 = tf.placeholder(shape=[1,75],dtype=tf.float32)
+
+''' possible actions :     
+    0 = south
+    1 = north
+    2 = east
+    3 = west
+    4 = pickup
+    5 = dropoff
+'''
+W = tf.Variable(tf.random_uniform([75,6],0,0.01))
+Qout = tf.matmul(inputs1,W)
+predict = tf.argmax(Qout,1)
+
+#Below we obtain the loss by taking the sum of squares difference between the target and prediction Q values.
+nextQ = tf.placeholder(shape=[1,6],dtype=tf.float32)
+loss = tf.reduce_sum(tf.square(nextQ - Qout))
+trainer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+updateModel = trainer.minimize(loss)
+
+# train network
+init = tf.initialize_all_variables()
+# Set learning parameters
+y = .99
+e = 0.1
+num_episodes = 2000
+epsilon = 0.01
+#create lists to contain total rewards and steps per episode
+jList = []
+rList = []
+with tf.Session() as sess:
+    sess.run(init)
+    for i in range(num_episodes):
+        #Reset environment and get first new observation
+        state = env.reset()
+
+        epochs, penalties, reward, = 0, 0, 0
+        done = False
+        
+        while not done:
+            if random.uniform(0, 1) < epsilon:
+                action = env.action_space.sample() # Explore action space
+            else:
+                action = np.argmax(q_table[state]) # Exploit learned values
+                a,allQ = sess.run([predict,Qout],feed_dict={inputs1:np.identity(25)[s:s+1]})
+
+            next_state, reward, done, info = env.step(action) 
+        
+        s = env.reset()
+        rAll = 0
+        d = False
+        j = 0
+        #The Q-Network
+        while j < 99:
+            j+=1
+            #Choose an action by greedily (with e chance of random action) from the Q-network
+            a,allQ = sess.run([predict,Qout],feed_dict={inputs1:np.identity(25)[s:s+1]})
+            if np.random.rand(1) < e:
+                a[0] = env.action_space.sample()
+            #Get new state and reward from environment
+            s1,r,d,_ = env.step(a[0])
+            #Obtain the Q' values by feeding the new state through our network
+            Q1 = sess.run(Qout,feed_dict={inputs1:np.identity(16)[s1:s1+1]})
+            #Obtain maxQ' and set our target value for chosen action.
+            maxQ1 = np.max(Q1)
+            targetQ = allQ
+            targetQ[0,a[0]] = r + y*maxQ1
+            #Train our network using target and predicted Q values
+            _,W1 = sess.run([updateModel,W],feed_dict={inputs1:np.identity(25)[s:s+1],nextQ:targetQ})
+            rAll += r
+            s = s1
+            if d == True:
+                #Reduce chance of random action as we train the model.
+                e = 1./((i/50) + 10)
+                break
+        jList.append(j)
+        rList.append(rAll)
+print("Percent of succesful episodes: " + str(sum(rList)/num_episodes))
+
+'''
+
+#initialize Q table
+q_table = np.zeros([env.observation_space.n, env.action_space.n])
+
+# Hyperparameters
+alpha = 0.1
+gamma = 0.6
+epsilon = 0.01
+
+# For plotting metrics
+all_epochs = []
+all_penalties = []
+
+for i in range(1, 100001):
+    state = env.reset()
+
+    epochs, penalties, reward, = 0, 0, 0
+    done = False
+    
+    while not done:
+        if random.uniform(0, 1) < epsilon:
+            action = env.action_space.sample() # Explore action space
+        else:
+            action = np.argmax(q_table[state]) # Exploit learned values
+
+        next_state, reward, done, info = env.step(action) 
+        
+        old_value = q_table[state, action]
+        next_max = np.max(q_table[next_state])
+        
+        new_value = (1 - alpha) * old_value + alpha * (reward + gamma * next_max)
+        q_table[state, action] = new_value
+
+        if reward == -10:
+            penalties += 1
+
+        state = next_state
+        epochs += 1
+        
+    if i % 100 == 0:
+        #clear_output(wait=True)
+        print(f"Episode: {i}",end="\r")
+print("",end="\n")
+print("Training finished.\n")
+
+"""Evaluate agent's performance after Q-learning"""
+
+total_epochs, total_penalties = 0, 0
+episodes = 100
+
+for _ in range(episodes):
+    state = env.reset()
+    epochs, penalties, reward = 0, 0, 0
+    
+    done = False
+    
+    while not done:
+        action = np.argmax(q_table[state])
+        state, reward, done, info = env.step(action)
+
+        if reward == -10:
+            penalties += 1
+
+        epochs += 1
+
+    total_penalties += penalties
+    total_epochs += epochs
+
+print(f"Results after {episodes} episodes:")
+print(f"Average timesteps per episode: {total_epochs / episodes}")
+print(f"Average penalties per episode: {total_penalties / episodes}")
+
+'''
